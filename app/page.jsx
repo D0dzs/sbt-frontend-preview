c7308f48d1e32786eeab05484eaf960759fedf42
Dodzs <gameeaters9@gmail.com>
2024-10-25 17:06:50 +0200

    refactor: base files, types, fonts and project structure

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus distinctio, optio ut aut excepturi doloremque temporibus dolor autem molestiae
+      minima.
+    </div>
+  );
+}
