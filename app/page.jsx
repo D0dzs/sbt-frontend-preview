4c1f32e5d0c29eb52036d79281255a11e8bbaacb
Dodzs <gameeaters9@gmail.com>
2024-11-24 18:14:45 +0100

    fix: parallax on mobile now "disabled", added dev:network to scripts

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,18 @@
-import { HeroParallax } from "~/components/(hero)/HeroParallax";
+import Image from 'next/image';
+import { HeroParallax } from '~/components/(hero)/HeroParallax';
 
 export default function Home() {
   return (
     <>
       <HeroParallax />
-      <div className="bg-black">
-        <img src={'/parallax/rock.webp'} className="-translate-y-12 lg:-translate-y-28 w-full h-fit" alt="rocks" />
+      <div className="bg-black relative">
+        <Image
+          src="/parallax/rock.webp"
+          className="z-50 absolute -top-20 lg:block lg:-translate-y-28 lg:w-full lg:h-fit"
+          alt="rocks"
+          width={2000}
+          height={411}
+        />
         <section className="relative min-h-screen px-8 lg:px-0 bg-black text-neutral-50"></section>
       </div>
     </>
--- components/(hero)/HeroParallax.jsx
+++ components/(hero)/HeroParallax.jsx
@@ -1,13 +1,14 @@
 'use client';
 
-import { MotionConfig, useScroll, useTransform, motion } from 'motion/react';
 import { useRef } from 'react';
 import { PARALLAX_LAYER_STYLES } from '~/utils/constants';
+import { MotionConfig, useScroll, useTransform, motion } from 'motion/react';
 import { HeroMouseIndicator } from './HeroMouseIndicator';
 
 export const HeroParallax = () => {
   const containerRef = useRef(null);
   const { scrollY } = useScroll();
+  const isMobile = typeof window !== 'undefined' ? window.innerWidth <= 768 : false;
 
   const y0 = useTransform(scrollY, [0, 1000], [0, 1000]);
   const y1 = useTransform(scrollY, [0, 1000], [0, 550]);
@@ -22,15 +23,15 @@ export const HeroParallax = () => {
   return (
     <MotionConfig reducedMotion={'user'}>
       <div ref={containerRef} className="relative h-screen w-full overflow-hidden">
-        <motion.div style={{ y: y0, ...PARALLAX_LAYER_STYLES, backgroundImage: 'url(/parallax/0.webp)' }} />
-        <motion.div style={{ y: y8, ...PARALLAX_LAYER_STYLES, backgroundImage: 'url(/parallax/8.webp)' }} />
-        <motion.div style={{ y: y7, ...PARALLAX_LAYER_STYLES, backgroundImage: 'url(/parallax/7.webp)' }} />
-        <motion.div style={{ y: y6, ...PARALLAX_LAYER_STYLES, backgroundImage: 'url(/parallax/6.webp)' }} />
-        <motion.div style={{ y: y5, ...PARALLAX_LAYER_STYLES, backgroundImage: 'url(/parallax/5.webp)' }} />
-        <motion.div style={{ y: y4, ...PARALLAX_LAYER_STYLES, backgroundImage: 'url(/parallax/4.webp)' }} />
-        <motion.div style={{ y: y3, ...PARALLAX_LAYER_STYLES, backgroundImage: 'url(/parallax/3.webp)' }} />
-        <motion.div style={{ y: y2, ...PARALLAX_LAYER_STYLES, backgroundImage: 'url(/parallax/2.webp)' }} />
-        <motion.div style={{ y: y1, ...PARALLAX_LAYER_STYLES, backgroundImage: 'url(/parallax/1.webp)' }} />
+        <motion.div style={{ y: isMobile ? 0.01 : y0, ...PARALLAX_LAYER_STYLES, backgroundImage: 'url(/parallax/0.webp)' }} />
+        <motion.div style={{ y: isMobile ? 0.01 : y8, ...PARALLAX_LAYER_STYLES, backgroundImage: 'url(/parallax/8.webp)' }} />
+        <motion.div style={{ y: isMobile ? 0.01 : y7, ...PARALLAX_LAYER_STYLES, backgroundImage: 'url(/parallax/7.webp)' }} />
+        <motion.div style={{ y: isMobile ? 0.01 : y6, ...PARALLAX_LAYER_STYLES, backgroundImage: 'url(/parallax/6.webp)' }} />
+        <motion.div style={{ y: isMobile ? 0.01 : y5, ...PARALLAX_LAYER_STYLES, backgroundImage: 'url(/parallax/5.webp)' }} />
+        <motion.div style={{ y: isMobile ? 0.01 : y4, ...PARALLAX_LAYER_STYLES, backgroundImage: 'url(/parallax/4.webp)' }} />
+        <motion.div style={{ y: isMobile ? 0.01 : y3, ...PARALLAX_LAYER_STYLES, backgroundImage: 'url(/parallax/3.webp)' }} />
+        <motion.div style={{ y: isMobile ? 0.01 : y2, ...PARALLAX_LAYER_STYLES, backgroundImage: 'url(/parallax/2.webp)' }} />
+        <motion.div style={{ y: isMobile ? 0.01 : y1, ...PARALLAX_LAYER_STYLES, backgroundImage: 'url(/parallax/1.webp)' }} />
 
         <HeroMouseIndicator scrollY={scrollY} />
       </div>
