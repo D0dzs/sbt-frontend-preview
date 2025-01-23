7cb8bcdef1d442ca6d0f92173d5a9093e6278897
Dodzs <gameeaters9@gmail.com>
2025-01-23 18:59:29 +0100

    chore: update dependencies, remove unused components, and modify layout
    - Added `autoprefixer` as a new dev dependency.
    - Updated `postcss` and `tailwindcss` to their latest versions.
    - Removed unused components: `Footer`, `Navbar`, `HeroMouseIndicator`, `HeroParallax`, and `constants.js`.
    - Updated the layout title in `layout.jsx`.
    - Simplified the home page to display a "Work In Progress" message.
    - Deleted the `about` page as part of the cleanup.

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,7 @@
-import Image from 'next/image';
-import { HeroParallax } from '~/components/(hero)/HeroParallax';
-
 export default function Home() {
   return (
-    <>
-      <HeroParallax />
-      <div className="bg-black relative">
-        <Image
-          src="/parallax/rock.webp"
-          className="z-50 absolute -top-20 lg:block lg:-translate-y-28 lg:w-full lg:h-fit"
-          alt="rocks"
-          width={2000}
-          height={411}
-        />
-        <section className="relative min-h-screen px-8 lg:px-0 bg-black text-neutral-50"></section>
-      </div>
-    </>
+    <div className="mx-auto h-screen flex items-center bg-bme-black">
+      <h1 className="text-center text-bme-white text-5xl font-bold w-full">Work In Progress</h1>
+    </div>
   );
 }
--- utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const PARALLAX_LAYER_STYLES = {
-  position: 'absolute',
-  width: '100%',
-  height: '100vh',
-  backgroundSize: 'cover',
-  backgroundPosition: 'center',
-  backgroundAttachment: 'fixed',
-};
