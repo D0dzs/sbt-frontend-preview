70fa3d13d17fe77cf154730b3d2f3cd71ac6e837
Dodzs <gameeaters9@gmail.com>
2025-02-03 05:41:00 +0100

    feat: enhance home page with dynamic background and styling
    - Added a dynamic, rotated background
    - Updated page layout to use grid and improve responsiveness
    - Applied Comfortaa font to the main heading (temporarly)
    - Added z-index and overflow handling for better visual composition
    - Updated .gitignore to exclude VSCode settings

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,17 @@
 export default function Home() {
   return (
-    <div className="mx-auto h-screen flex items-center bg-bme-black">
-      <h1 className="text-center text-bme-white text-5xl w-full">
-        Work in progress...
-      </h1>
+    <div className="mx-auto h-screen grid bg-bme-black relative overflow-hidden items-center">
+      {/* background */}
+      <div
+        style={{ transform: 'rotate(45deg) translate(0%, 70%) ' }}
+        className="w-[1500px] h-[1500px] bg-bme-purple absolute bottom-0 flex items-center select-none blur-3xl rounded-2xl"
+      >
+        <div className="relative h-[1000px] w-[1000px] bg-bme-yellow/65 mx-auto" />
+      </div>
+      {/* real content */}
+      <div id="container" className="z-10">
+        <h1 className="text-center text-bme-white text-5xl w-full font-comfortaa font-semibold">Work in progress...</h1>
+      </div>
     </div>
   );
 }
