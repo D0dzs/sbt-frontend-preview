94d71b9c6f9b0da7ba3068fee7b10c3eadfc124a
Dodzs <51715950+D0dzs@users.noreply.github.com>
2025-02-05 09:10:40 +0000

    resolve pr

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,21 @@
-import Navbar from './components/Navbar';
+import HeroScreen from './components/(Hero)/HeroPage';
+import SectionLayout from './components/(Layouts)/SectionLayout';
+import Container from './components/Container';
+import { ModeToggle } from './components/Theme-Button';
 
 export default function Home() {
   return (
-    <div className="mx-auto grid bg-[url(/hero/background.png)] items-center justify-center h-screen">
-      <Navbar />
+    <div className="mx-auto grid items-center justify-center overflow-x-hidden relative">
+      {/* Navbar */}
+      <HeroScreen />
+      <SectionLayout className={'h-[200vh] p-8'}>
+        <ModeToggle />
+        <Container className={'w-[900px] h-[400px] grid'}>
+          <div> left </div>
+          <div> right </div>
+        </Container>
+      </SectionLayout>
+      {/* Footer */}
     </div>
   );
 }
