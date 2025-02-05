d6037cf1b6eb6e158825772e63644cc5ff3650c0
Abai Bence <134051380+Gaba38@users.noreply.github.com>
2025-02-05 09:05:28 +0000

    Navbar edit:
    -background blur
    -drop shadow
    -background image

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,22 +4,24 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className="bg-bme-white/50 w-[75vw] h-24 rounded-full px-8 ">
-      <div className="text-bme-white text-3xl grid grid-flow-col items-center justify-between ">
-        <div className="flex items-center">
-          <Link href={''}>
-            <Image src={'/navbar/logo.svg'} width={96} height={96} alt="Logo" />
-          </Link>
-          <div className="flex gap-4">
-            <Link href={''}>Rólunk</Link>
-            <Link href={''}>Hírek</Link>
-            <Link href={''}>Galéria</Link>
-            <Link href={''}>Szponzorok</Link>
+    <div>
+      <div className=" bg-linear-to-t from-bme-white/50 to-bme-white/30 w-[75vw] h-24 rounded-full px-8 backdrop-blur-sm drop-shadow-lg">
+        <div className="text-bme-white text-3xl grid grid-flow-col items-center justify-between ">
+          <div className="flex items-center gap-15">
+            <Link href={''}>
+              <Image src={'/navbar/logo.svg'} width={96} height={96} alt="Logo" />
+            </Link>
+            <div className="flex gap-4">
+              <Link href={''}>Rólunk</Link>
+              <Link href={''}>Hírek</Link>
+              <Link href={''}>Galéria</Link>
+              <Link href={''}>Szponzorok</Link>
+            </div>
           </div>
+          <Link href={''} className="bg-bme-orange text-bme-black text-center rounded-full px-4 py-2">
+            Kapcsolat
+          </Link>
         </div>
-        <Link href={''} className="bg-bme-orange text text-center rounded-full px-4">
-          Kapcsolat
-        </Link>
       </div>
     </div>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,8 @@ import Navbar from './components/Navbar';
 
 export default function Home() {
   return (
-    <div className="mx-auto grid bg-bme-black items-center justify-center h-screen">
+    <div className="mx-auto grid bg-[url(/hero/background.png)] items-center justify-center h-screen">
       <Navbar />
-      <h1 className="text-center text-5xl font-poppins font-semibold bg-gradient-to-r from-bme-purple to-bme-orange p-2 text-transparent bg-clip-text">
-        Work in progress...
-      </h1>
     </div>
   );
 }
