4b98f7b6fb1b943bb6b778908c5033159aa3391c
Gaba38 <abence050714@gmail.com>
2025-02-09 18:32:12 +0100

    refactor(Footer): update styling and layout for improved visual design
    - Adjusted footer container classes for better responsiveness
    - Modified background and text colors to match new design system
    - Refined grid layout and added shadow and rounded corners
    -Prepared for light and dark theme

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -5,24 +5,24 @@ const Footer = () => {
   return (
     <footer className="mx-auto w-[70vw] py-8 pt-32">
       <div className="bg-bme-lsecondary text-bme-black dark:text-bme-white dark:bg-bme-dsecondary relative grid overflow-clip rounded-2xl shadow-xl">
-        <div className="grid justify-items-center h-fit w-[70vw] grid-cols-4 p-0 lg:w-[60vw] lg:grid-flow-col lg:p-8">
-          <div className="grid h-fit w-[70vw] grid-cols-1 gap-2 p-0 lg:w-[10vw]  ">
-            <img src="./images/logo/secondarylogodark.svg" />
+        <div className="">
+          <div className="">
+            <img src="" />
             <p>info@solarboatteam.hu</p>
             <p>+36 70 608 7004</p>
           </div>
-          <div className="grid gap-2">
+          <div className="">
             <Link className='text font-bold' href={""}>Rólunk</Link>
             <Link href={""}>Csapattagok</Link>
             <Link href={""}>Rólunk+</Link>
             <Link href={""}>Eredményeink</Link>
           </div>
-          <div className="grid gap-2">
+          <div className="">
             <Link className='text font-bold' href={""}>Hírek</Link>
             <Link href={""}>Sajtó</Link>
             <Link href={""}>Blog</Link>
           </div>
-          <div className="grid gap-2">
+          <div className="">
             <Link className='text font-bold' href={""}>Egyéb</Link>
             <Link href={""}>Galéria</Link>
             <Link href={""}>Szponzorok</Link>
