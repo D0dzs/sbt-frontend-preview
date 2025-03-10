bbcddbc849df86f174b50f48f3103ac5ec2ba6c6
Dodzs <gameeaters9@gmail.com>
2025-03-10 17:11:22 +0100

    revert env vars...

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
     ],
     formats: ['image/webp'],
   },
+  env: {
+      API_URL: 'https://sbt-backend-demo-production.up.railway.app/api',
+      CDN_URL: 'https://sbt-backend-demo-production.up.railway.app/cdn',
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
